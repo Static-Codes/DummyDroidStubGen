8d19d4a003a0dd3740197dee1b997493f36d6838
Static <210101922+Static-Codes@users.noreply.github.com>
2026-06-11 17:33:21 -0400

    Fixed connection regex pattern to properly match devices connected through usb hubs.

--- src/Core/Common/RegexPatterns.cs
+++ src/Core/Common/RegexPatterns.cs
@@ -10,6 +10,6 @@ public static partial class RegexPatterns
     [GeneratedRegex(@"([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})")] 
     public static partial Regex ValidateAddressIPV4();
 
-    [GeneratedRegex(@"(?<USBDeviceID>[0-9A-Z]{14})\s{1,}device\s{1,}usb:[0-9]{1,}-[0-9]{1,}\s{1,}product:\w{1,}\s{1,}model:(?<USBDeviceName>\w{1,})\s{1,}device:(?<USBDeviceCodename>\w{1,})\s{1,}transport_id:(?<USBTransportID>\w{1,})|(?<IP>[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}):(?<Port>[0-9]{1,5})\s{1,5}device\s{1,}product:\w{1,}\s{1,}model:(?<WIFIDeviceName>\w{1,})\s{1,}device:(?<WIFIDeviceCodename>\w{1,})\s{1,}transport_id:(?<WIFITransportID>\w{1,})")]
+    [GeneratedRegex(@"(?<USBDeviceID>[0-9A-Z]{14})\s{1,}device\s{1,}usb:[0-9]{1,}-[0-9]{1,}.*product:\w{1,}\s{1,}model:(?<USBDeviceName>\w{1,})\s{1,}device:(?<USBDeviceCodename>\w{1,})\s{1,}transport_id:(?<USBTransportID>\w{1,})|(?<IP>[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}):(?<Port>[0-9]{1,5})\s{1,5}device\s{1,}product:\w{1,}\s{1,}model:(?<WIFIDeviceName>\w{1,})\s{1,}device:(?<WIFIDeviceCodename>\w{1,})\s{1,}transport_id:(?<WIFITransportID>\w{1,})")]
     public static partial Regex ConnectionRegex();
 }
